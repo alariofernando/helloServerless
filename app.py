3091cf0598582aa5dcf8c0ea5bfac68dd9b3c6aa
Fernando Pires <alariofernandopires@gmail.com>
2020-06-17 22:51:23 -0300

    Name path

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 # app.py
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/<name>")
+def name(name):
+    return f"Hello {name.capitalize()}"
