67aa00b6d9404449a8e91c0c25034b050b152644
FERNANDO ALARIO PIRES <servicoiteris738113@userede.com.br>
2020-07-23 11:04:22 -0300

    CORS

--- app.py
+++ app.py
@@ -7,8 +7,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"body": "Hello World", "statusCode": 200})
+    return jsonify({
+        "body": "Hello World",
+        "headers": {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'},
+        "statusCode": 200
+        })
 
 @app.route("/<name>")
 def name(name):
-    return jsonify({"body": f"Hello {name.capitalize()}", "statusCode": 200})
+    return jsonify({
+        "body": f"Hello {name.capitalize()}",
+        "headers": {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'},
+        "statusCode": 200
+        })
