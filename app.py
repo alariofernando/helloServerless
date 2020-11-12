106dcdfa0478967494c9f3895919738855febf8c
Fernando Alario Pires <fernando.pires@iteris.com.br>
2020-11-12 20:47:29 -0300

    Catherine

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     if name := request.args.get('nome'):
+        if "catherine" in name.lower():
+            body = "Ahhh, você é o amor da vida do Fernando <3 <3"
         body = f"O nome que você colocou é {name}"
     else:
         body = "Você não colocou nenhum nome ;-;"
